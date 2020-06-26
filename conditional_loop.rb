1a9b5fcdc59a20d93c5ca2d2b81c5505b29b47b1
Kevin Bacon <kjbacon13@gmail.com>
2020-06-26 15:14:50 +0900

    First commit to launch school repo

---
+++ conditional_loop.rb
@@ -0,0 +1,12 @@
+process_the_loop = [true, false].sample
+
+if process_the_loop == true
+
+  puts "The loop was processed!"
+
+else
+
+  puts "The loop was not processed!"
+
+end
+
---
+++ empty_the_array.rb
@@ -0,0 +1,9 @@
+names = ['Sally', 'Joe', 'Lisa', 'Henry']
+
+loop do 
+
+break if names.length == 0
+
+    puts names.pop
+
+end
